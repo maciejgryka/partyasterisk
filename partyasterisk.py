5897e1226f2135bd2e3988bd389a67f3d82429b2
Maciej Gryka <maciej.gryka@gmail.com>
2016-04-16 23:05:21 +0200

    made an app work!

--- partyasterisk.py
+++ partyasterisk.py
@@ -1,24 +1,49 @@
 from __future__ import print_function
 
 import os
+from uuid import uuid4
 
-from flask import Flask, url_for
 import matplotlib.pyplot as plt
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+from werkzeug import secure_filename
 
 from imageparty import throw_party
 
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploads')
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg',])
+
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/')
-def hello():
-    img = plt.imread('data/jonathan.png')
-    path = 'apitest.gif'
-    with open(os.path.join('static', path), 'wb') as out_file:
-        throw_party(out_file, img)
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-    return '<!DOCTYPE html><img src="%s" alt="party hard"/>' % (url_for('static', filename=path))
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            img = plt.imread(filepath)
+            out_path = str(uuid4()) + '.gif'
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], out_path), 'wb') as out_file:
+                throw_party(out_file, img)
+            return redirect(url_for('uploaded_file', filename=out_path))
+
+    return render_template('index.html')
+
+
+@app.route('/party')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
